e4fc499a5e7cd17dca287c6f837c9971ad07df29
Dan <danreader0@gmail.com>
2020-09-14 22:04:57 -0400

    build and publish the game

---
+++ build/precache-manifest.377da125f8dceb19dafd13423c762101.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85886e3b98ceced043adff4d2b495ba1",
+    "url": "/deathroll/index.html"
+  },
+  {
+    "revision": "e0aebb4ed1d1fcda272c",
+    "url": "/deathroll/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "148dabf9811b029843e5",
+    "url": "/deathroll/static/css/main.ebc79e31.chunk.css"
+  },
+  {
+    "revision": "e0aebb4ed1d1fcda272c",
+    "url": "/deathroll/static/js/2.e3eb01be.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/deathroll/static/js/2.e3eb01be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148dabf9811b029843e5",
+    "url": "/deathroll/static/js/main.5352ed9c.chunk.js"
+  },
+  {
+    "revision": "eb842f4660ef6c8673ab",
+    "url": "/deathroll/static/js/runtime-main.5e03fe8d.js"
+  },
+  {
+    "revision": "173dc85d0cdd288bffb05602a5b51565",
+    "url": "/deathroll/static/media/BalooDa2-SemiBold.173dc85d.ttf"
+  },
+  {
+    "revision": "7c26c2a7102569f75f664025acd21c91",
+    "url": "/deathroll/static/media/concreteWall.7c26c2a7.png"
+  }
+]);
