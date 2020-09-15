a74fbb62347dcd1d07879a8a3d9d8d17188fd9cd
Dan <danreader0@gmail.com>
2020-09-14 22:45:37 -0400

    version update

--- src/components/Titlebar/Titlebar.js
+++ src/components/Titlebar/Titlebar.js
@@ -6,7 +6,8 @@ const TitleBar = () => {
     <Container>
       <Row>
         <Col>
-          <h1 className="mt-5 mb-3" style={{color : '#F3F3F3', textShadow: '#000000AA -1px 2px'}}>deathroll</h1>
+          <h1 className="mt-5 mb-3" style={{ color: '#F3F3F3', textShadow: '#000000AA -1px 2px' }}>deathroll</h1>
+          <h3 className="mt-5 mb-3" style={{color : '#F3F3F3', textShadow: '#000000AA -1px 2px'}}>v0.5.0</h3>
         </Col>
       </Row>
     </Container>
